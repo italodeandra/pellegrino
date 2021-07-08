bb5f89582f2b69c934497144329249d9fc8a1745
Ítalo Andrade <italodeandra@gmail.com>
2021-07-08 21:25:39 +0200

    fix: check if socket is available before setting up the hooks

--- lib/setupSchemaHooks.ts
+++ lib/setupSchemaHooks.ts
@@ -1,15 +1,17 @@
 import { MongooseDocumentMiddleware, Schema } from "mongoose";
 
 export default function setupSchemaHooks(name: string, schema: Schema<any>) {
-  schema.post(
-    [
-      "updateOne",
-      "findOneAndUpdate",
-      "save",
-      "updateMany",
-    ] as MongooseDocumentMiddleware[],
-    function () {
-      global.io.to(`model/${name}`).emit("refetch");
-    }
-  );
+  if (global.io) {
+    schema.post(
+      [
+        "updateOne",
+        "findOneAndUpdate",
+        "save",
+        "updateMany",
+      ] as MongooseDocumentMiddleware[],
+      function () {
+        global.io.to(`model/${name}`).emit("refetch");
+      }
+    );
+  }
 }
